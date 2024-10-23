3bca958e95d54c5ee3bf01eca3446c5e084da21b
Bouisr <romain.bouis@viacesi.fr>
2024-10-23 11:04:16 +0200

    avancement front

--- djBackPlaylist/src/server.js
+++ djBackPlaylist/src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // CFG static files
 app.use(express.static(path.join(__dirname, 'public')));
 
