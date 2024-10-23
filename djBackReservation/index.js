ba257b9e35e4b4541a91e1c74451928a9245a87e
kevinFichou <66632429+kemrode@users.noreply.github.com>
2024-10-23 14:05:55 +0200

    feat(putreservation): ajout des fonctions PUT & PATCH

--- djBackReservation/index.js
+++ djBackReservation/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const cors = require('cors');
 
 //CFG PORT
 const PORT = process.env.PORT || 3002;
+
+app.use(cors());
 
 const bookJson = "booking.json";
 const bookingJson = require(`./${bookJson}`);
@@ -20,7 +23,6 @@ app.get("/booking/:id", (req, res) => {
     try {
         var id = req.params.id;
         var data = bookingJson.soirees.find(s => s.id == id);
-        console.log(data);
         res.status(200).json(data);
     } catch (error) {
         res.status(400);
@@ -30,7 +32,6 @@ app.get("/booking/:id", (req, res) => {
 
 app.post("/booking", express.json(), (req, res) => {
     try {
-        console.log(req.body);
         bookingJson.soirees.push(req.body)
         fs.writeFile(bookJson, JSON.stringify(bookingJson), (err) => {
             if (err) {
@@ -48,10 +49,36 @@ app.post("/booking", express.json(), (req, res) => {
 
 app.put("/booking/:id", express.json(), (req, res) => {
     try {
-        res.status(201)
+        const id = req.params.id;
+        var soireeToModify = bookingJson.soirees.find(s => s.id == id);
+        soireeToModify.nom = req.body.nom;
+        soireeToModify.date = req.body.date;
+        soireeToModify.playliste = req.body.playliste;
+        soireeToModify.client = req.body.client;
+        res.status(200).json(soireeToModify);
     } catch (error) {
         res.status(400);
         console.log(error);
+    }
+});
+
+app.patch('/booking/:id', express.json(), (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(req.params)
+        var soireeToPatch = bookingJson.soirees.find(s => s.id == id);
+        soireeToPatch.playliste = req.body.playliste;
+        fs.writeFile(bookJson, JSON.stringify(bookingJson), (err) => {
+            if (err) {
+                console.log(`L'écriture dans le JSON a échoué, sa mère la teup: ${err}`);
+                throw err;
+            }
+            console.log("L'écriture dans le JSON s'est bien passé");
+        })
+        res.status(200).json(bookingJson);
+    } catch (error) {
+        res.status(400);
+        console.log(`T'as fait de la merde, connard : ${error}`);
     }
 });
 
