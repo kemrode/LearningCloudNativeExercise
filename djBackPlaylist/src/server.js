66ba2761f98085aadca9f39d9cb77c29bc784f16
kevinFichou <66632429+kemrode@users.noreply.github.com>
2024-10-24 16:45:27 +0200

    feat(checkconsul): mise en place du docker-compose avec healthcheck et consul ok

--- djBackPlaylist/src/server.js
+++ djBackPlaylist/src/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const playlistRoutes = require('./router/playlist');
 const path = require('path');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+
+// const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
 // CFG static files
@@ -20,10 +23,28 @@ app.use(function (request, response, next) {
 app.use(express.json());
 app.use('/playlists', playlistRoutes);
 
-const consul = require('consul');
+const Consul = require('consul');
 const consulName = "api-playlist";
+const os = require('os');
+const networkAdress = os.networkInterfaces();
+const containerIp = networkAdress['eth0'][0].address;
 
-await consul.Agent.Service.register({ name: consulName, id: 123456789, address: "http://api-playlist", port: 3001 });
+const registerAPI = async () => {
+  var id = uuidv4();
+  const consul = new Consul({ host: 'consul' });
+  await consul.agent.service.register(
+    {
+      name: consulName,
+      id: id,
+      address: containerIp,
+      port: 3001,
+      check: {
+        http: `http://${containerIp}:3001/check`,
+        interval: "15s",
+        timeout: "45s"
+      }
+    });
+}
 
 app.get('/', (req, res) => {
   res.sendFile(patch.join(__dirname, 'public', 'index.html'));
@@ -41,4 +62,5 @@ app.get('/check', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  registerAPI();
 });
